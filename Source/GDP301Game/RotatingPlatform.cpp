16ec3678ba3513fb78e0e5c170719282ef12897c
Charlie Campbell <87422910+HotdogsCC@users.noreply.github.com>
2026-05-25 10:38:25 +1000

    debugging rotation

---
+++ Source/GDP301Game/RotatingPlatform.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RotatingPlatform.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ARotatingPlatform::ARotatingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARotatingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARotatingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	if (!PlayerController)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to get the player controller"));
+		return;
+	}
+	
+	FVector Tilt;
+	FVector RotationRate;
+	FVector Gravity;
+	FVector Acceleration;
+	PlayerController->GetInputMotionState(Tilt, RotationRate, Gravity, Acceleration);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Tilt | X:%f, Y:%f, Z:%f"), Tilt.X, Tilt.Y, Tilt.Z);
+	UE_LOG(LogTemp, Warning, TEXT("RotationRate | X:%f, Y:%f, Z:%f"), RotationRate.X, RotationRate.Y, RotationRate.Z);
+	UE_LOG(LogTemp, Warning, TEXT("Gravity | X:%f, Y:%f, Z:%f"), Gravity.X, Gravity.Y, Gravity.Z);
+	UE_LOG(LogTemp, Warning, TEXT("Acceleration | X:%f, Y:%f, Z:%f"), Acceleration.X, Acceleration.Y, Acceleration.Z);
+}
+
